05e109d273dea3f5da4681a4676254ab245fec81
Tal <tal@maimonator.dev>
2023-04-04 13:13:20 +0300

    Update to version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # GPU Monitor
+Allows you to monitor the usage of your NVIDIA GPU directly in your status bar.
+
+Note: this extension has no affiliation with NVIDIA.
+
+## Features
+- Displays the name, memory usage, and temperature of your Nvidia GPU in the status bar.
+- Allows you to select the GPU you want to monitor.
+- Allows you to set the update interval for monitoring the GPU.
+- Shows an error message if the nvidia-smi command is not found on your system or if the specified GPU index cannot be found.
+- Provides a tooltip with additional information about GPU utilization and memory utilization.
+
+## Requirements
+- This extension requires the nvidia-smi command to be installed on your system.
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+- `gpu-monitor.updateInterval`: the interval (in milliseconds) at which to update the GPU information. Defaults to 1000.
+- `gpu-monitor.selectedGpuIndex`: the index of the GPU to monitor. Defaults to 0 (the first GPU).
+- `gpu-monitor.binaryPath`: the path to the nvidia-smi command. Defaults to "nvidia-smi".
+
+## Commands
+This extension contributes the following commands:
+
+- `gpu-monitor.selectGpu`: allows you to select the GPU you want to monitor.
+- `gpu-monitor.setUpdateInterval`: allows you to set the update interval for monitoring the GPU.
+
+## Credits
+
+- README was generated with ChatGPT
+- Icon was generated with Dall-E
+- Code was co-authored with Bing chat
--- src/extension.ts
+++ src/extension.ts
@@ -16,6 +16,39 @@ let myStatusBarItem: vscode.StatusBarItem;
 let selectedGpuIndex = 0;
 let updateInterval = 1000;
 let updateIntervalId: NodeJS.Timeout;
+let binaryPath = "nvidia-smi";
+
+
+function getNvidiaSMIError(): Promise<string | undefined> {
+    return new Promise((resolve, reject) => {
+        let error = exec(binaryPath, (error, stdout, stderr) => {
+            if (error) {
+                resolve(`The command ${binaryPath} was not found on shell.`);
+            }
+        });
+        error = exec(`${binaryPath} -i ${selectedGpuIndex}`, (error, stdout, stderr) => {
+            if (error) {
+                resolve(`Could not find GPU with index ${selectedGpuIndex} make sure you have a GPU available`);
+            } else {
+                resolve(undefined);
+            }
+        });
+    });
+}
+
+function testNvidiaSMI() {
+    getNvidiaSMIError().then(error => {
+        if (error) {
+            vscode.window.showErrorMessage(`Extension startup failed because of '${error}' try and update your settings`, "Open Settings").then(choice => {
+                if (choice === "Open Settings") {
+                    vscode.commands.executeCommand('workbench.action.openSettings', '@ext:Maimonator.gpu-monitor');
+                }
+            });
+        }
+    });
+
+}
+
 
 
 export function activate(context: vscode.ExtensionContext) {
@@ -23,37 +56,48 @@ export function activate(context: vscode.ExtensionContext) {
     myStatusBarItem.command = 'gpu-monitor.selectGpu';
     context.subscriptions.push(myStatusBarItem);
 
-    let disposable = vscode.commands.registerCommand('gpu-monitor.selectGpu', async () => {
-        const gpus = await getGpus();
-        const gpuNames = gpus.map(gpu => gpu.name);
+    context.subscriptions.push(vscode.commands.registerCommand('gpu-monitor.selectGpu', async () => {
+        const gpuNames = await getGpuNames();
         const selectedGpuName = await vscode.window.showQuickPick(gpuNames, { placeHolder: 'Select a GPU' });
         if (selectedGpuName) {
             selectedGpuIndex = gpuNames.indexOf(selectedGpuName);
         }
-    });
-    context.subscriptions.push(disposable);
+    }));
 
-    disposable = vscode.commands.registerCommand('gpu-monitor.setUpdateInterval', async () => {
+    context.subscriptions.push(vscode.commands.registerCommand('gpu-monitor.setUpdateInterval', async () => {
         const intervalString = await vscode.window.showInputBox({ placeHolder: 'Enter the update interval in milliseconds' });
         if (intervalString) {
             updateInterval = parseInt(intervalString);
             clearInterval(updateIntervalId);
             updateIntervalId = setInterval(updateGpuMemory, updateInterval);
         }
-    });
-    context.subscriptions.push(disposable);
+    }));
+
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(event => {
+        if (event.affectsConfiguration('gpu-monitor')) {
+            const configuration = vscode.workspace.getConfiguration('gpu-monitor');
+            updateInterval = configuration.get('updateInterval', 1000);
+            selectedGpuIndex = configuration.get('selectedGpuIndex', 0);
+            binaryPath = configuration.get('binaryPath', "nvidia-smi");
+            testNvidiaSMI();
+        }
+
+    }));
 
     const configuration = vscode.workspace.getConfiguration('gpu-monitor');
     updateInterval = configuration.get('updateInterval', 1000);
+    selectedGpuIndex = configuration.get('selectedGpuIndex', 0);
+    binaryPath = configuration.get('binaryPath', "nvidia-smi");
 
+
+    testNvidiaSMI();
     updateGpuMemory();
     updateIntervalId = setInterval(updateGpuMemory, updateInterval);
 }
 
-
-async function getGpus(): Promise<Gpu[]> {
+async function getGpuNames(): Promise<string[]> {
     return new Promise((resolve, reject) => {
-        exec('nvidia-smi --query-gpu=name,memory.used,memory.total,temperature.gpu,utilization.gpu,utilization.memory,clocks.current.graphics,clocks.current.memory --format=csv,noheader', (error, stdout, stderr) => {
+        exec(`${binaryPath} --query-gpu=name --format=csv,noheader`, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
                 return;
@@ -62,24 +106,40 @@ async function getGpus(): Promise<Gpu[]> {
                 reject(stderr);
                 return;
             }
-            const lines = stdout.split('\n').slice(0, -1);
-            const gpus = lines.map(line => {
-                const [name, memoryUsed, memoryTotal, temperature, gpuUtilization, memoryUtilization, graphicsClock, memoryClock] = line.split(', ');
-                return { name, memoryUsed, memoryTotal, temperature, gpuUtilization, memoryUtilization, graphicsClock, memoryClock };
-            });
-            resolve(gpus);
+            const gpuNames = stdout.split('\n').slice(0, -1).map(line => line.trimEnd().trimStart());
+            resolve(gpuNames);
+        });
+    });
+}
+
+async function getGpuData(): Promise<Gpu> {
+    return new Promise((resolve, reject) => {
+        exec(`${binaryPath} -i ${selectedGpuIndex} --query-gpu=name,memory.used,memory.total,temperature.gpu,utilization.gpu,utilization.memory,clocks.current.graphics,clocks.current.memory --format=csv,noheader`, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            if (stderr) {
+                reject(stderr);
+                return;
+            }
+            const gpuData = stdout.split('\n')[0];
+            const [name, memoryUsed, memoryTotal, temperature, gpuUtilization, memoryUtilization, graphicsClock, memoryClock] = gpuData.split(', ');
+            let gpu = { name, memoryUsed, memoryTotal, temperature, gpuUtilization, memoryUtilization, graphicsClock, memoryClock };
+            resolve(gpu);
         });
     });
 }
 
 async function updateGpuMemory() {
-    const gpus = await getGpus();
-    if (gpus.length > 0) {
-        const gpu = gpus[selectedGpuIndex];
+    const gpu = await getGpuData();
+    if (gpu !== null) {
         myStatusBarItem.text = `${gpu.name}: ${gpu.memoryUsed} / ${gpu.memoryTotal}, ${gpu.temperature}°C`;
         myStatusBarItem.tooltip = `GPU Utilization: ${gpu.gpuUtilization}\nMemory Utilization: ${gpu.memoryUtilization}\nGraphics Clock: ${gpu.graphicsClock}\nMemory Clock: ${gpu.memoryClock}`;
         myStatusBarItem.show();
     }
 }
 
-export function deactivate() {}
+export function deactivate() {
+    myStatusBarItem.hide();
+}
